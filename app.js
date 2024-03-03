107cd56bbca4bbf1463c7a3af4d642093dc54a10
{Sameer Khan} <{sam3131084526@gmail.com}>
2024-03-03 15:47:12 +0500

    Java script assigment

---
+++ app.js
@@ -0,0 +1,25 @@
+alert("Error! Please enter a valid password");
+
+alert("Welcome to JS Land...\nHappy Coding");
+
+alert("Welcome to JS Land...");
+
+alert("Happy Coding!\n Prevent this page from creating additional dialog");
+
+console.log("Hello... I can run JS through my web browser's console");
+
+var username;
+
+var myName = "Sameer Khan";
+
+var message;
+
+message = "Hello World";
+
+alert(message);
+
+alert(myName);
+
+var age = 15;
+
+alert(age+ " "+"years"+ " "+"old");
